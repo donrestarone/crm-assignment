28ed03ffe1ffacda8b00e99faab356766c446dc9
donrestarone <shashikejayatunge@gmail.com>
2018-03-28 17:13:42 -0400

    finish contact class

--- contact.rb
+++ contact.rb
@@ -87,10 +87,10 @@ class Contact
   end 
 
  
-  def self.find_by_first_name(first_n) 
+  def self.find_by_first_name(first_name) 
     obj = 0
     @@contacts.each do |contact|
-      if contact.first_name == first_n    #contact block variable calls on first_name method's return value. 
+      if contact.first_name == first_name    #contact block variable calls on first_name method's return value. 
         obj =1                            #use of obj is the same as before.
         return contact
       end
@@ -100,18 +100,23 @@ class Contact
     end 
   end 
 
-  # This method should delete all of the contacts
+
   def self.delete_all
+    @@contacts = []
+    return @@contacts
+  end 
 
-  end
 
-  # This method should delete the contact
-  # HINT: Check the Array class docs for built-in methods that might be useful here
-  def delete
+  def self.deletebyname(first_n) 
+    @@contacts.delete_if {|contact| contact.first_name == first_n} #when given argument first_n, it goes through the array
+  end                                                              #and invokes built in delete_if method. block variable contact
+                                                                  #calls on the reader method first_name, and if the argument matches the object, its deleted. 
 
-  end
 
-  # Feel free to add other methods here, if you need them.
+
+
+
+
   
 end
 
@@ -121,34 +126,34 @@ diana = Contact.new('diana', 'prince', 'diana@amazon.com') #set default value fo
 #create works 
 bruce = Contact.create('Bruce', 'Wayne', 'bruce@wayne.com', 'dark guy') #saves it by default 
 
-# puts clark.inspect
-# puts diana.inspect
-# puts bruce.inspect
+puts clark.inspect
+puts diana.inspect
+puts bruce.inspect
 
 diana.save
 clark.save
 
 puts Contact.all.inspect
 
-# puts
-# puts Contact.all.inspect 
+puts
+puts Contact.all.inspect 
 
-# puts 
-# #testing readers 
-# puts diana.full_name.inspect #print full name works 
-# puts diana.email.inspect #print email works 
-# puts clark.notes.inspect
-# puts diana.notes.inspect
+puts 
+#testing readers 
+puts diana.full_name.inspect #print full name works 
+puts diana.email.inspect #print email works 
+puts clark.notes.inspect
+puts diana.notes.inspect
 
-# #testing writers
-# clark.first_name=('lark')
-# puts clark.inspect
-# diana.last_name=('Prince')
-# puts diana.inspect
-# clark.email=('clark@gmail.com')
-# puts clark.inspect
-# diana.notes=('royal')
-# puts diana.inspect
+#testing writers
+clark.first_name=('lark')
+puts clark.inspect
+diana.last_name=('Prince')
+puts diana.inspect
+clark.email=('clark@gmail.com')
+puts clark.inspect
+diana.notes=('royal')
+puts diana.inspect
 
 # puts 
 # puts
@@ -158,6 +163,18 @@ puts Contact.all.inspect
  puts 
 
 puts Contact.find_by_first_name("diefgergeaa").inspect
+
+#Contact.delete_all
+puts
+puts
+
+ puts Contact.all.inspect
+puts
+ puts
+ Contact.deletebyname("diana")
+puts 
+puts
+ puts Contact.all.inspect
 
 
 
