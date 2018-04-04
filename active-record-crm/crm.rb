5f099f7a17bf20ed1aed7b94bfab91189ef2c873
donrestarone <shashikejayatunge@gmail.com>
2018-04-04 16:03:35 -0400

    done CRM for now, needs a lot of conditional logic for polish and UX

--- active-record-crm/crm.rb
+++ active-record-crm/crm.rb
@@ -13,7 +13,7 @@ class CRM
       print_main_menu
       user_selected = gets.chomp.to_i
       
-      if user_selected == 6
+      if user_selected == 0
         i = 1
       end 
 
@@ -28,8 +28,11 @@ class CRM
     puts "[3] Delete a contact"
     puts "[4] Display all the contacts"
     puts "[5] Search by ID"
-    puts "[6] Exit"
-    puts "enter a number"
+    puts "[6] Search by attribute"
+    puts "[7] delete contact"
+    puts "[8] Exit"
+    puts "[9] Exit"
+    puts "[0] EXIT]"
 
   end
 
@@ -40,6 +43,8 @@ class CRM
       when 3 then delete_contact
       when 4 then display_all_contacts
       when 5 then search_by_id
+      when 6 then search_by_attribute
+      when 7 then delete_contact
     end 
   end
 #add_new contact calls on Contact class which then calls on the create method in the active_record class 
@@ -67,20 +72,58 @@ class CRM
     p search
   end 
 
+  def search_by_attribute 
+    p "enter the attribute you want to search by. OPTIONS: first_name, last_name, email, note"
+    input = gets.chomp
+    p "enter the search query"
+    search_query = gets.chomp
+    search_result = Contact.find_by(input => search_query )
+    p "--------------------------"
+      p "ID:#{search_result.id} NAME:#{search_result.full_name}"
+      
+      p "INFORMATION:#{search_result.rest}"
+    p "--------------------------"
+  end 
+
   def display_all_contacts
     all_contacts = Contact.all
     all_contacts.each do |contact|
       p "--------------------------"
-      p "name:#{contact.full_name}"
+      p "ID:#{contact.id} NAME:#{contact.full_name}"
       
-      p "information:#{contact.rest}"
+      p "INFORMATION:#{contact.rest}"
       p "--------------------------"
     end 
-
-
-
-    # p all_contacts
   end 
+
+  def delete_contact
+    p "enter the ID of the contact you would like to delete"
+    id = gets.chomp 
+    search_result = Contact.find(id)
+    p "press y to confirm"
+    anykey = gets.chomp.to_s
+    if anykey == "y"
+      search_result.delete 
+      p "contact deleted successfully"
+    else 
+      p "error"
+    end 
+    
+  end 
+
+  def modify_existing_contact
+  p "enter the id of the contact you want to edit"
+  id = gets.chomp 
+  desired_contact = Contact.find(id) #desired contact grabs the object that matches the ID 
+
+  p "what do you want to update. OPTIONS: first_name, last_name, email, note "
+  input = gets.chomp 
+  p "what content would you like to add?"
+  content = gets.chomp
+
+  desired_contact.update(input => content ) #if input matches the key in the array, it updates that key with the content. 
+  end
+   
 
 
 
