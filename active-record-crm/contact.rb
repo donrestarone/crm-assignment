9574a1d5efcf3a8e6c650b5a78041acf649cd457
donrestarone <shashikejayatunge@gmail.com>
2018-04-04 14:42:48 -0400

    started integrating active record to crm

--- active-record-crm/contact.rb
+++ active-record-crm/contact.rb
@@ -1,186 +1,20 @@
-class Contact
-  @@contacts = []
-  @@next_id = 1000
-  def initialize(first_name, last_name, email, notes = 'N/A')
-    @first_name = first_name
-    @last_name = last_name
-    @email = email 
-    @notes = notes
+gem 'activerecord', '=4.2.10' #when we are using active record make sure its this version, not the other version we have installed in our machine. 
+require 'active_record'
+require 'mini_record'
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'crm.sqlite3')
 
-    @id = @@next_id
-    @@next_id += 1
-
-  end
-
-  def self.create(first_name, last_name, email, notes = 'N/A')
-    new_contact = Contact.new(first_name, last_name, email, notes)
-    new_contact.save
-    return new_contact
-  end
-
-  def save
-    @@contacts.push self 
-  end
-
-  def self.all
-    return @@contacts
-  end 
-
-  def id_reader
-    return @id
-  end
-
-  def self.find(id)
-    obj=0 #obj starts at zero
-    @@contacts.each do |contact|
-      if contact.id_reader == id  #contact.id_reader; here contact block variable calls on the return value of id_reader
-          obj =1                  # if the condition is met, obj becomes 1, if not its zero. being zero triggers the error
-         return contact         
-      end
-
-        if obj !=1
-          return  "contact not found"
-        end
-    end 
-  end
-
-  def first_name #reader
-    @first_name
-  end 
-
-  def last_name ##reader
-    @last_name
-  end 
-
-  def email #reader
-    @email
-  end 
-
-  def notes #reader
-    @notes
-  end 
-
+class Contact < ActiveRecord::Base
+  field :first_name, as: :string 
+  field :last_name , as: :string 
+  field :email, as: :text 
+  field :note, as: :text 
+  
   def full_name
     "#{first_name} #{last_name}"
-
   end
 
-  def update
-
-  end
-
-  def first_name=(first_name) #writer
-    @first_name = first_name
-  end 
-
-  def last_name=(last_name) #writer
-    @last_name = last_name
-  end 
-
-  def email=(email) #writer
-    @email = email
-  end 
-
-  def notes=(notes) #writer
-    @notes = notes 
-  end 
-
- 
-  def self.find_by_first_name(first_name) 
-    obj = 0
-    # p first_name
-    @@contacts.each do |contact|
-      # p contact.first_name
-      if contact.first_name == first_name    #contact block variable calls on first_name method's return value. 
-        obj = 1     
-        #p contact.first_name                       #use of obj is the same as before.
-        return contact
-      end
-    end 
-    if obj != 1 
-       return "error"
-     end 
-  end 
-
-
-  def self.delete_all
-    @@contacts = []
-    return @@contacts
-  end 
-
-
-
-  def delete
-    @@contacts.delete(self) #delete using built in method delete
-  end 
-                                                              #and invokes built in delete_if method. block variable contact
-                                                                  #calls on the reader method first_name, and if the argument matches the object, its deleted. 
-  
 end
-
-#testing create 3 people. 
-# clark = Contact.new('Clark', 'kent', 'clark@clark.com', 'hello')
-# diana = Contact.new('diana', 'prince', 'diana@amazon.com') #set default value for the notes argument, check other file 
-# #create works 
-# bruce = Contact.create('Bruce', 'Wayne', 'bruce@wayne.com', 'dark guy') #saves it by default 
-
-# puts clark.inspect
-# puts diana.inspect
-# puts bruce.inspect
-
-# diana.save
-# clark.save
-
-# puts Contact.all.inspect
-
-# puts
-# puts Contact.all.inspect 
-
-# puts 
-# #testing readers 
-# puts diana.full_name.inspect #print full name works 
-# puts diana.email.inspect #print email works 
-# puts clark.notes.inspect
-# puts diana.notes.inspect
-
-# #testing writers
-# clark.first_name=('Clark')
-# puts clark.inspect
-# diana.last_name=('Prince')
-# puts diana.inspect
-# clark.email=('clark@gmail.com')
-# puts clark.inspect
-# diana.notes=('royal')
-# puts diana.inspect
-
-# # puts 
-# # puts
-#  puts Contact.find(1005).inspect
-
-#  puts 
-#  puts 
-
-# puts Contact.find_by_first_name("diana").inspect
-
-# #Contact.delete_all
-# puts
-# puts
-
-#  puts Contact.all.inspect
-# puts
-#  puts
-#  # Contact.deletebyname("diana")
-# puts 
-# puts
-#  puts Contact.all.inspect
-
-
-# puts clark.id_reader.inspect
-
-# #clark.delete
-
-
-#  puts Contact.all.inspect
+Contact.auto_upgrade!
 
 
 
--- active-record-crm/crm.rb
+++ active-record-crm/crm.rb
@@ -8,10 +8,17 @@ class CRM
   end
 
   def main_menu
-    while true 
+    i = 0
+    while i != 1  
       print_main_menu
       user_selected = gets.chomp.to_i
+      
+      if user_selected == 6
+        i = 1
+      end 
+
       call_option(user_selected)
+
     end
   end
 
@@ -33,72 +40,7 @@ class CRM
       when 3 then delete_contact
       when 4 then display_all_contacts
       when 5 then search_by_attribute
-      when 6 then raise "quit "
     end 
-  end
-
-  def add_new_contact
-    p "enter first name"
-    first_name = gets.chomp 
-    p "enter last name"
-    last_name = gets.chomp 
-    p "enter email"
-    email = gets.chomp 
-    p "enter notes about the person"
-    notes = gets.chomp
-
-    Contact.create(first_name, last_name, email, notes) 
-  end
-
-  def modify_existing_contact
-    p "modify works"
-  end
-
-  def delete_contact
-    p "enter the first name"
-      answer = gets.chomp.to_s
-    found_contact = Contact.find_by_first_name(answer)
-    p found_contact
-    found_contact.delete
-  end
-
-  def display_all_contacts
-    Contact.all.each do |contact|
-    puts "+++++++++++++++++++++++++++++++++++"
-    puts "+++++++++++++++++++++++++++++++++++"
-    puts "name: #{contact.full_name}"
-    puts "----------------------------------"
-    puts "email: #{contact.email}"
-    puts "----------------------------------"
-    puts "notes: #{contact.notes}"
-    puts "----------------------------------"
-    puts "unique ID: #{contact.id_reader}"
-    puts "+++++++++++++++++++++++++++++++++++"
-    puts "+++++++++++++++++++++++++++++++++++"
-    end
-
-  end
-
-  # def search_by_attribute  #search by first name 
-  #   p "enter first name"
-  #   enter_name = gets.chomp
-  #   Contact.find_by_first_name(enter_name)
-    
-  # end
-
-  def search_by_attribute  #search by first name 
-    p "enter first name"
-    enter_name = gets.chomp
-    contact = Contact.find_by_first_name(enter_name)
-     puts "name: #{contact.full_name}"
-    puts "----------------------------------"
-    puts "email: #{contact.email}"
-    puts "----------------------------------"
-    puts "notes: #{contact.notes}"
-    puts "----------------------------------"
-    puts "unique ID: #{contact.id_reader}"
-   
-    return 
   end
 
 end
@@ -106,4 +48,8 @@ end
 menue = CRM.new
 
 puts menue.main_menu
+
+at_exit do
+  ActiveRecord::Base.connection.close
+end
 
