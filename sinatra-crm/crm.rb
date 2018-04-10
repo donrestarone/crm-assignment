f51566bda03e0a1597ff4af19815a0b9fe6b9198
donrestarone <shashikejayatunge@gmail.com>
2018-04-10 15:59:31 -0400

    new.erb route works

--- sinatra-crm/crm.rb
+++ sinatra-crm/crm.rb
@@ -24,16 +24,24 @@ get '/about' do
 	erb :about
 end 
 
+get '/contacts/new' do
+	p "hi"
+	@title = 'Create new Contact'
+	erb :new
+end 
+
 get '/contacts/:id' do
 	id = params[:id].to_i
 	@contact = Contact.find_by({id: id})	#@contact gets assigned the result of find_by method in active record. find_by takes hash as argument. 
-	
+	@title = 'contact:'+ @contact.full_name
 	if @contact
 		erb :show_contact
 	else 
 		raise Sinatra::NotFound 
 	end 
 end 
+
+
 
 # get '/contacts'
 
